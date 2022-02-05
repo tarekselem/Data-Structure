6aa5653a6e1f1267b755d8ceb7c38ac210a83730
Tarek Selem <t.hamdy91@yahoo.com>
2022-02-05 13:43:51 +0100

    Initial commit

---
+++ dist/lib/array.js
@@ -0,0 +1,11 @@
+"use strict";
+// static size
+// insert()
+// print()
+// removeAt()
+// indexOf()
+exports.__esModule = true;
+init();
+function init() {
+    console.log(12121212);
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { ArrayData } from './lib';
+
+const myData = new ArrayData<number>(2);
+console.log('hi!');
